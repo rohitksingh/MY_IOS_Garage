a2e757d2b105345d014b36c5a811cf889be72078
Rohit <r4rohit002@gmail.com>
2020-01-31 13:18:24 -0700

    Initial Commit

---
+++ IOS Lifecycle/IOS Lifecycle/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  IOS Lifecycle
+//
+//  Created by Rohit  on 31/01/20.
+//  Copyright © 2020 Rohit . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func loadView() {
+        super.loadView()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
